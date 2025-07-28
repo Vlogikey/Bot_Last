b47209cc3f3f61731e3862fcb289aec838a0d6b1
Vlogikey <vlogikey@gmail.com>
2025-07-29 01:32:30 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Конфигурация Telegram бота
+BOT_TOKEN = "7983764759:AAHods3z0lDWP2rmQIE9FOZUajLG3GyNVls"  # Ваш реальный токен
+ADMIN_ID = 7757791357  # Ваш ID в Telegram (число без кавычек)
+
+# Дополнительные настройки (если нужны)
+MOSCOW_TZ = "Europe/Moscow"
